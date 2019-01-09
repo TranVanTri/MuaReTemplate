105da985eab93da8715a0fc4ea253b79ce265a37
TranVanTri <tvtri1997@gmail.com>
2019-01-09 10:12:16 +0700

    tạo hiệu ứng slide cho sản phẩm độc đáo

--- assets/js/myJS.js
+++ assets/js/myJS.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+
+	// Responnse giao dien trang chu
 	$(window).resize(function(){
 		if($(window).width()<=1200){
 		  	$('.content-left').removeClass('col-lg-9 col-md-9');
@@ -6,6 +8,32 @@ $(document).ready(function() {
 		}else {
 			$('.content-left').addClass('col-lg-9 col-md-9');
 		  	$('.content-left').removeClass('col-lg-12 col-md-12');
+		}
+	});
+	// owl-carousel sản phẩm độc đáo
+	$('.promoteProduct.row.owl-carousel').owlCarousel({
+		loop:true,
+		autoplayHoverPause:true,
+		autoplay:true,
+		autoplayTimeout:3000,
+		responsiveClass:true,
+		responsive:{
+			0:{
+				items:2,
+				nav:true,
+				loop:true,
+				
+			},
+			600:{
+				items:3,
+				nav:true,
+				loop:true
+			},
+			1000:{
+				items:4,
+				nav:true,
+				loop:true,
+			}
 		}
 	});
 });
